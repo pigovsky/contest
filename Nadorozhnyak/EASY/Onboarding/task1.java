4841553005b75dacd1ab2f9e35535f467cca11af
SheggyN <nvcompany2009@gmail.com>
2016-09-19 13:11:52 +0300

    Add files via upload

---
+++ Nadorozhnyak/EASY/Onboarding/task1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Player {
+    public static void main(String args[]) { 
+		Scanner in = new Scanner(System.in);
+        
+		while (true) {
+			String enemy1 = in.next();
+			int dist1 = in.nextInt();
+			String enemy2 = in.next();
+			int dist2 = in.nextInt();
+
+			if(dist1<dist2)         
+				System.out.println(enemy1);
+			else
+				System.out.println(enemy2);
+		}
+	}
+}
---
+++ Nadorozhnyak/EASY/Power of Thor/PowerOfThor.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class Player {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		int lightX = in.nextInt();
+		int lightY = in.nextInt();
+		int initialTX = in.nextInt();
+		int initialTY = in.nextInt();
+
+		int X=lightX-initialTX, Y=lightY-initialTY;
+
+		while (true) {
+			int remainingTurns = in.nextInt();
+			String s = "";
+			if(Y<0){
+				s+="N";
+				Y++;
+			}
+			else if(Y>0){
+				s+="S";
+				Y--;
+			}
+
+			if(X<0){
+				s+="W";
+				X++;
+			}
+			else if(X>0){
+				s+="E";
+				X--;
+			}
+
+			System.out.println(s);
+		}
+	}
+}
